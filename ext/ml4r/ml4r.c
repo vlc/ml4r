9dff7041f43e918d5eaef3fe1c0769b1eabcc106
Jamie Cook <jamie.cook@veitchlister.com.au>
2012-06-24 16:01:00 +1000

    working in a regression example

--- ext/ml4r/ml4r.c
+++ ext/ml4r/ml4r.c
@@ -1,17 +1,23 @@
 #include <ruby.h>
+#include "LinearRegression/LinearRegression.h"
 
-/* our new native method; it just returns
- * the string "bonjour!" */
-static VALUE tce_hello_world(VALUE self) {
-  return rb_str_new2("hello world");
+VALUE rb_cRegression;
+
+void Init_ZenithRegression()
+{
+    rb_cRegression = otRuby->rb_define_class("ZenithRegression", rb_cObject());
+    otRuby->rb_define_singleton_method(rb_cRegression, "new", zenith_regression_New), -1);
+    otRuby->rb_define_method(rb_cRegression, "initialize",            zenith_regression_Initialize, 0);
+    otRuby->rb_define_method(rb_cRegression, "observations=",         zenith_regression_observations, 1);
+    otRuby->rb_define_method(rb_cRegression, "weights=",              zenith_regression_weights, 1);
+    otRuby->rb_define_method(rb_cRegression, "setFixedConstant=",     zenith_regression_setFixedConstant, 1);
+    otRuby->rb_define_method(rb_cRegression, "fittedYs",              zenith_regression_getFittedYs, 0);
+    otRuby->rb_define_method(rb_cRegression, "predictedYs",           zenith_regression_getPredictedYs, 0);
+    otRuby->rb_define_method(rb_cRegression, "regressionStatistics",  zenith_regression_getRegressionStatistics, 0);
+    otRuby->rb_define_method(rb_cRegression, "execute",               zenith_regression_execute, 0);
 }
 
 /* ruby calls this to load the extension */
 void Init_ml4r(void) {
-  /* assume we haven't yet defined TestCppExtension */
-  VALUE klass = rb_define_class("TestCppExtension", rb_cObject);
-
-  /* the hola_bonjour function can be called
-   * from ruby as "Hola.bonjour" */
-  rb_define_singleton_method(klass, "bonjour", tce_hello_world, 0);
+    Init_ZenithRegression
 }
