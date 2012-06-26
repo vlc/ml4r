e0a593b82e69cdd2719f59973ab342f7952114a9
Jamie Cook <jamie.cook@veitchlister.com.au>
2012-06-26 21:59:48 +1000

    finally got swig working

---
+++ ext/ml4r/example.cpp
@@ -0,0 +1,10 @@
+// example.cpp
+// #include "example.h"
+
+double Foo = 3.0;
+
+int fact(int n) {
+ if (n == 0)
+ return 1;
+ return (n * fact(n-1));
+}
---
+++ ext/ml4r/example.i
@@ -0,0 +1,8 @@
+%module ml4r
+%{
+extern double Foo;
+int fact(int n);
+%}
+
+extern double Foo;
+int fact(int n);
