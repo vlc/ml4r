5e2a4e7f4923d5ba276aae33290a6f5dbd72fb70
Jamie Cook <jamie.cook@veitchlister.com.au>
2012-06-26 21:44:27 +1000

    test script

--- test/test_compilation.rb
+++ test/test_compilation.rb
@@ -1,9 +1,18 @@
 require 'test/unit'
 p $:.inspect
-require 'ml4r'
+#require 'ml4r'
+
+#class TestCompilation < Test::Unit::TestCase
+#  def test_hello_world
+#    assert_equal("hello world", TestCppExtension.bonjour())
+#  end
+#end
+
+require 'ml4r/example'
 
 class TestCompilation < Test::Unit::TestCase
   def test_hello_world
-    assert_equal("hello world", TestCppExtension.bonjour())
+    p Module.constants
+    assert_equal("hello world", ::Example.fact(5))
   end
 end
