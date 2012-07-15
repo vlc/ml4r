b15aede6f8a0a8c28233a1f7c88c01a254313b49
Jamie Cook <jamie.cook@veitchlister.com.au>
2012-07-15 14:32:26 +1000

    added auto increment of version number, cleaned up ignores

---
+++ tasks/gem_tasks.rb
@@ -0,0 +1,15 @@
+def increment_gemspec_version
+  dat = IO.readlines("ml4r.gemspec")
+  version = nil
+  File.open("ml4r.gemspec", 'w') { |f| 
+    dat.each { |line|
+      if line =~ /s.version     = '(\d+).(\d+).(\d+)'/
+        version = "#{$1}.#{$2}.#{$3.to_i+1}"
+        f.puts(  "s.version     = '#{version}'")
+      else
+        f.puts(line)
+      end
+    }
+  }
+  return version
+end
