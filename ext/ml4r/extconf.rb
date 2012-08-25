8a086b3cf6ff599fa667bb7097cbbd90b0dcb69e
Jamie Cook <jamie.cook@veitchlister.com.au>
2012-08-25 21:46:03 +1000

    makefile should compile to the subdir where the cpp resides

--- ext/ml4r/extconf.rb
+++ ext/ml4r/extconf.rb
@@ -9,3 +9,8 @@ $srcs = Dir.glob("**/*.c*")
 $objs = $srcs.map { |e| e.gsub(/cpp$/, "o") }
 
 create_makefile('ml4r/ml4r')
+
+dat = IO.readlines("Makefile")
+File.open("Makefile", 'w') { |f|
+  dat.each { |line| f.puts(line.gsub(/\$\</, "$< -o $@")) }
+}
