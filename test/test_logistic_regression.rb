fdcc20de0f5b4face5fef4eb2714629b877ba8a0
Jamie Cook <jamie.cook@veitchlister.com.au>
2014-03-30 15:37:49 +1000

    indentation

--- test/test_logistic_regression.rb
+++ test/test_logistic_regression.rb
@@ -3,8 +3,8 @@ require 'ml4r/ml4r'
 
 # class Array
 #   def to_double_vector
-#   	v = Ml4r::DoubleVector.new
-#   	self.each { |e| v << e }
+#     v = Ml4r::DoubleVector.new
+#     self.each { |e| v << e }
 #   end
 # end
 
@@ -12,24 +12,24 @@ class TestLogisticRegression < Test::Unit::TestCase
   DELTA = 0.0001
 
   def setup
-	@data = Ml4r::MLData.new
-	
-	xs = [[1.0,2.0],[1.0,3.0],[1.0,4.0],[1.0,5.0],[1.0,6.0]]
-	ys = [    0,        0,        1,        0,         1]
-	ws = [    1,        1,        1,        1,         1]
-	
-	xs.zip(ys).each { |x,y|
-	  @data.addObservation(x,y)
-	}
+    @data = Ml4r::MLData.new
+
+    xs = [[1.0,2.0],[1.0,3.0],[1.0,4.0],[1.0,5.0],[1.0,6.0]]
+    ys = [    0,        0,        1,        0,         1]
+    ws = [    1,        1,        1,        1,         1]
+
+    xs.zip(ys).each { |x,y|
+      @data.addObservation(x,y)
+    }
   end
-  
+
   def test_everything
     lr = Ml4r::LogisticRegressionRunner.new
-  	assert(lr.is_a?(Ml4r::LogisticRegressionRunner))
-  	assert(lr.is_a?(Ml4r::MLRunner))
-  	lr.setData(@data)
-  	lr.execute
-  	predictions = lr.getMeanTrainingPredictions
-  	p predictions
+    assert(lr.is_a?(Ml4r::LogisticRegressionRunner))
+    assert(lr.is_a?(Ml4r::MLRunner))
+    lr.setData(@data)
+    lr.execute
+    predictions = lr.getMeanTrainingPredictions
+    p predictions
   end
 end
